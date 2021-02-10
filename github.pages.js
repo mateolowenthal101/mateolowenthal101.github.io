813b41daba353dd373aa5146bc9f0013239f7e25
mateolowenthal101 <studentmateolowenthal101@gmail.com>
2021-02-09 20:18:57 -0600

    test

--- github.pages.js
+++ github.pages.js
@@ -1,1 +1,10 @@
 //jk
+var ghpages = require('gh-pages');
+
+ghpages.publish('dist/static', {
+    branch: 'gh-pages',
+    repo: 'git@github.com:mateolowenthal101/mateolowenthal101.github.io.git'
+  },  (err)=>{
+      if (err) console.log(err);
+      console.log("did it");
+  });
